dc0461a6a68bf61d717ee32c217a0e9b877c9fe5
Aafke <aafkegroenveld@hotmail.com>
2017-01-02 20:05:19 +0100

    First argument when run is used as path for the file to be validated. Improved exception handling and boyscout.

--- src/main/java/RabobankCustomerStatementProcessor.java
+++ src/main/java/RabobankCustomerStatementProcessor.java
@@ -1,40 +1,38 @@
 import org.apache.commons.io.FilenameUtils;
 
 import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class RabobankCustomerStatementProcessor {
-    public static void main(String[] args) throws IOException {
-        Validator validator = new Validator();
-        List<Transaction> transactionList = new ArrayList<>();
+    public static void main(String[] args) {
 
         //todo:get from args which file should be read:
         //get from console which file should be read:
-        String path = "getStringFromStart.xml";
+        String path = args[0];
 
         //create Importer based on extension
         TransactionImporter transactionImporter = ImporterFactory.getImporter(FilenameUtils.getExtension(path));
 
-        //todo: add proper catch blocks.
-        //todo: decide how to get file to be validated
         //get list with transaction in file
+        List<Transaction> transactionList;
         try {
-            transactionList = transactionImporter.loadFromFile("addFullPathNameOfFile");
-        } catch (IOException exception){
+            transactionList = transactionImporter.loadFromFile(path);
+
+            //Validate transactions and write invalid transactions to report.
+            Validator validator = new Validator();
+            if(transactionList != null){
+                List<Transaction> invalidTransactions = validator.validateTransactions(transactionList);
+
+                ReportWriter.writeToReport(invalidTransactions);
+            }
+        } catch (FileNotFoundException exception){
             System.out.println("File was not found");
         } catch (JAXBException e) {
             System.out.println("JAXBException");
+        } catch (IOException exception){
+            System.out.println("Oeps, could not read or write content");
         }
-
-        //Validate transactions and write invalid transactions to report.
-        if(transactionList != null){
-            //validate transactions
-            List<Transaction> invalidTransactions = validator.validateTransactions(transactionList);
-
-            ReportWriter.writeToReport(invalidTransactions);
-        }
-
     }
 }
